1de7e5cd105dde2c457b8f8081d0c0693e883d71
pradeo123 <prasad.s.deokar@gmail.com>
2022-04-13 18:26:40 +0530

    Add all my files

---
+++ src/test/java/com/example/MySpringBoot/MySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
